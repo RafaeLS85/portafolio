9242e1a61001f1f539432647116030cf7c6418c9
RafaeLS85 <rafaelsebastian@live.com.ar>
2021-02-15 13:31:39 -0300

    agregado rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeadersComponent } from './shared/headers/headers.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadersComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
